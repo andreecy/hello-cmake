0895599dc0389f74ba3e7e4b32aa73f952e82f1d
Andree CY <andreechristianyulianto@gmail.com>
2023-01-29 14:53:59 +0700

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+## Hello CMake
+
+Basic of creating C library and linking to project with CMake
---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h> 
+#include "mylib.h"
+
+int main() {
+	int result = add(5, 7);
+	printf("%d + %d = %d\n", 5, 7, result);
+	return 0;
+}
